3711f3139e04a19174589c03f8157d041497f565
Alfredo Adrian <alfredoadrian718@gmail.com>
2021-09-28 23:46:20 -0500

    Pruebas

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -3,8 +3,9 @@ module.exports = async function (context, req) {
 
   const name = req.query.name || (req.body && req.body.name);
   const responseMessage = name
-    ? "welcome, " + name
+    ? "welcome, " + name + "example"
     : " Pass a name in the query string or in the request body for a personalized response.";
+  context.log("JavaScript HTTP trigger 2.");
 
   context.res = {
     // status: 200, /* Defaults to 200 */
--- HttpTrigger/index.test.js
+++ HttpTrigger/index.test.js
@@ -8,6 +8,9 @@ test("Http trigger example", async () => {
 
   await httpFunctions(context, request);
   // expoct(context.res.body).toEqual("Hello, schuc");
-  expect(context.res.body).toEqual("welcome, schuc");
-  expect(context.log.mock.calls.length).toEqual(1);
+  expoct(context.res.body).toContain("example");
+  expect(context.res.body).toEqual("welcome, schuc example");
+
+  // expect(context.log.mock.calls.length).toEqual(1);
+  expect(context.log.mock.calls.length).toBe(1);
 });
